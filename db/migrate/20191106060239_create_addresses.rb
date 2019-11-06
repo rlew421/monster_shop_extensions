81b7a58ec005a4febe9b692ef92f2535dd18a5d4
Rachel Lew <rlew07@gmail.com>
2019-11-06 08:05:04 -0700

    Creates address table

---
+++ db/migrate/20191106060239_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :addresses do |t|
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.timestamps
+      t.string "location", default: "home"
+    end
+  end
+end
